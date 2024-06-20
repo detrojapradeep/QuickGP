155479d99a7e1af2b33b7214fdad998b155ab925
Pradeep Detroja <detrojapradeep111@gmail.com>
2024-06-20 18:32:25 +0100

    Bug Express api not working

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-import express from 'express'
-const app = express()
-const port = 5500
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = process.env.port || 5500;
+
+// Middleware to parse request body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+  
+app.post('/appointmentForm', (req, res) => {
+  console.log("Received POST request at /appointmentForm");
+});
 
 app.listen(port, () => {
-  console.log(`Ooooo bhai!! ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
